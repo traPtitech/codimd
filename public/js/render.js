df52d34b3bd6ff2e5733ade22c98bf533bec6a99
sapphi-red <green@sapphi.red>
2021-02-11 19:11:57 +0900

    accept iframe from trap.jp

--- public/js/render.js
+++ public/js/render.js
@@ -4,8 +4,26 @@
 var DOMPurify = require('dompurify')
 var filterXSS = require('xss')
 
+// https://dev.to/patarapolw/securely-embed-youtube-and-other-iframe-elements-in-markdown-2hoc
+DOMPurify.addHook('uponSanitizeElement', function (node, data) {
+  if (data.tagName === 'iframe') {
+    var src = node.getAttribute('src') || ''
+    try {
+      var url = new URL(src)
+      if (!url.hostname.endsWith('trap.jp')) {
+        return node.remove()
+      }
+    } catch (e) {
+      return node.remove()
+    }
+  }
+})
+
 function preventXSS (html) {
-  return DOMPurify.sanitize(html)
+  return DOMPurify.sanitize(html, {
+    ADD_TAGS: ["iframe"],
+    ADD_ATTR: ["scrolling", "frameborder"]
+  })
 }
 window.preventXSS = preventXSS
 
